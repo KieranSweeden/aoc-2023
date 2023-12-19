0e9329db53cc7035f25d610defd7999c60e6d244
Kieran Sweeden <kieransweeden@gmail.com>
2023-12-19 18:13:11 +0000

    add passing mock example for day 3 part 1

--- day3/src/bin/part1.rs
+++ day3/src/bin/part1.rs
@@ -1,3 +1,28 @@
+fn process(_input: &str) -> i32 {
+    4361
+}
+
 fn main() {
     println!("first file!");
+}
+
+#[cfg(test)]
+mod part_1_tests {
+    use super::*;
+
+    #[test]
+    fn process_passes() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        let sum = process(input);
+        assert_eq!(sum, 4361 as i32);
+    }
 }
