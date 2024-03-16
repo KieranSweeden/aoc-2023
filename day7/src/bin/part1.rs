df02fbc7f942d6cc456bb5d5a0805383e08c1eff
Kieran Sweeden <kieransweeden@gmail.com>
2024-03-16 10:38:12 +0000

    add remaining day 7 files

---
+++ day7/src/bin/part1.rs
@@ -0,0 +1,16 @@
+use std::str::FromStr;
+
+mod camel_cards;
+
+fn process(input: &str) -> Result<u64, camel_cards::error::Error> {
+    let game = camel_cards::game::Game::from_str(input)?;
+    Ok(game.calculate_winnings())
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+    match process(input) {
+        Ok(result) => println!("The result is: {}", result),
+        Err(e) => panic!("Error: {:?}", e),
+    }
+}
